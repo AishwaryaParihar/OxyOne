b7d50b9e3b7075bd76b17adec71ee768fed29ce5
testuser <testuser@gmail.com>
2024-08-23 18:40:10 +0530

    plant

--- src/component/content/PlantType.jsx
+++ src/component/content/PlantType.jsx
@@ -6,8 +6,8 @@ const PlantType = () => {
         <div className='w-full'>
             <div className="p-3">
                 <h3 className='text-2xl font-bold text-center'>Nature's <span className="text-primary">Oxygen Hero</span>es</h3>
-                <p className="text-xs text text-center font-medium  mt-2">Discover the magnificent plants that contribute the most to our environment by producing abundant oxygen, essential for life on Earth.</p>
-                <div className="flex flex-cols-2 py-6">
+                <p className="text-sm text text-center font-medium  mt-2">Discover the magnificent plants that contribute the most to our environment by producing abundant oxygen, essential for life on Earth.</p>
+                <div className="flex flex-cols-2 py-2 mt-4">
                     <div className="w-1/2 text-center text-xs bg-slate-100 hover:bg-green-100 p-2 rounded-2xl mr-2">
                         <div className="flex "><span><img src={tree} alt="" className='w-14 rounded-full ' /></span>
                             <span className='ps-3'>  <div className="text-lg text-primary font-semibold text-center">Jamun Tree</div>
@@ -22,6 +22,20 @@ const PlantType = () => {
                     </div>
                 </div>
                 <div className="flex flex-cols-2">
+                    <div className="w-1/2 text-center text-xs bg-slate-100 p-2 rounded-2xl mr-2">
+                        <div className="flex "><span><img src={tree} alt="" className='w-14 rounded-full ' /></span>
+                            <span className='ps-3'>  <div className="text-lg text-primary font-semibold text-center">Jamun Tree</div>
+                                <div className='text-sm'>Description</div>
+                            </span></div>
+                    </div>
+                    <div className="w-1/2 text-center text-xs bg-slate-100 p-2 rounded-2xl">
+                        <div className="flex "><span><img src={tree1} alt="" className='w-14 h-14 rounded-full ' /></span>
+                            <span className='ps-3'>  <div className="text-lg text-primary font-semibold text-center">Jamun Tree</div>
+                                <div className='text-sm'>Description</div>
+                            </span></div>
+                    </div>
+                </div>
+                <div className="flex flex-cols-2 mt-2">
                     <div className="w-1/2 text-center text-xs bg-slate-100 p-2 rounded-2xl mr-2">
                         <div className="flex "><span><img src={tree} alt="" className='w-14 rounded-full ' /></span>
                             <span className='ps-3'>  <div className="text-lg text-primary font-semibold text-center">Jamun Tree</div>
