7c38f07072bd8a197bb542cc0367d96ea8902abf
Karishma <karishmadawar123@gmail.com>
2024-08-23 17:47:17 +0530

    testimonial

--- src/component/content/Home.jsx
+++ src/component/content/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import LeaderBoard from './LeaderBoard'
 import Hero from './Hero'
-
+import Testimonials from './Testimonials'
 const Home = () => {
   return (
     <div >
       <Hero/>
       <LeaderBoard/>
+      <Testimonials/>
     </div>
   )
 }
