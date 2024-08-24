36e3e72d6e85bfab7a5379e98905f99a9b4015e7
Karishma <karishmadawar123@gmail.com>
2024-08-24 17:43:23 +0530

    images of hero section

--- src/component/content/Hero.jsx
+++ src/component/content/Hero.jsx
@@ -9,18 +9,21 @@ import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
-import carousel from "../../assets/desert.webp"; 
-import carousel2 from "../../assets/caraousel2.webp"; 
-import carousel3 from "../../assets/caraousel3.jpg"; 
+import carousel from "../../assets/carousel6.jpg"; 
+import carousel2 from "../../assets/desert.webp"; 
+import carousel3 from "../../assets/img7.jpg"; 
+import carousel4 from "../../assets/img3.jpg"; 
+
 const images = [
   carousel,
   carousel2,
   carousel3,
+  carousel4,
 ];
 
 const Hero = () => {
   return (
-    <div className="w-full  mx-auto mt-10 mb-6">
+    <div className="w-full  mx-auto mt-10 mb-6 py-10">
       <Swiper
         spaceBetween={30}
         slidesPerView={1}
@@ -36,7 +39,7 @@ const Hero = () => {
             <img
               src={src}
               alt={`Carousel Slide ${index + 1}`}
-              className="w-full h-[75vh] object-fill"
+              className="w-full h-[80vh] object-cover"
             />
           </SwiperSlide>
         ))}
