4f6562346f55dedf4a2416914a9f3cde65f675cf
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-24 12:42:50 +0530

    remove webview styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,14 @@ function App() {
   return (
     <>
  
- <div className="flex flex-col justify-center items-center bg-secondary">
-      <div className="lg:w-6/12 xl:w-[470px] sm:w-8/12 w-full min-h-screen border border-5 shadow-md shadow-zinc-300 relative">
-    
-        <div className="w-full h-full flex flex-col mt-3">
+ 
           {/* {children} */}
           {/* <Outlet/> */}
           <Navbar/>
           <Outlet/>
-        </div>
+     
        <Footer/>
-      </div>
-    </div>
+     
     </>
   )
 }
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -2,94 +2,44 @@ import React, { useState } from "react";
 import logo from "../../assets/oxyVan-logo.png";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
-    <nav className="bg-white shadow-md sticky z-20 top-0 start-0 border-b border-gray-200">
-      <div className="lg:w-6/12 xl:w-[470px] sm:w-8/12 w-full mx-auto flex items-center justify-between ">
-        {/* Logo */}
-        <a href="/" className="flex items-center">
-          <img src={logo} className="h-20" alt="Logo" />
-        </a>
+    
 
-        {/* Button */}
-        <button
-          type="button"
-          className="md:block text-white bg-second hover:bg-second-hover focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-9 mr-11 py-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800 transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
-        >
-          Donate
-        </button>
+<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto ">
+  <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src={logo} class="h-20" alt="Flowbite Logo"/>
 
-        {/* Hamburger Icon */}
-        <button
-  onClick={toggleMenu}
-  className="flex items-center mr-9 text-gray-500 rounded-lg focus:outline-none active:ring-0"
-  aria-controls="navbar-sticky"
-  aria-expanded={isOpen}
->
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
-            />
-          </svg>
-        </button>
-      </div>
+  </a>
+  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+  </div>
+  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <li>
+        <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
 
-      {/* Dropdown Menu with transition */}
-      <div
-        className={`transition-all duration-700 ease-linear transform ${
-          isOpen ? "opacity-100 max-h-screen" : "opacity-0 max-h-0 overflow-hidden"
-        } bg-white shadow-lg`}
-      >
-        <ul className="space-y-2 p-4">
-          <li>
-            <a
-              href="#"
-              className="block text-gray-900 hover:bg-gray-100 rounded py-2 px-4"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block text-gray-900 hover:bg-gray-100 rounded py-2 px-4"
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block text-gray-900 hover:bg-gray-100 rounded py-2 px-4"
-            >
-              Services
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block text-gray-900 hover:bg-gray-100 rounded py-2 px-4"
-            >
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
   );
 };
 
