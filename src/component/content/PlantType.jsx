8e51966111c24927d1a0fc6e60c9485786f17b0a
testuser <testuser@gmail.com>
2024-08-27 13:05:50 +0530

    plant type  responsivenes

--- src/component/content/PlantType.jsx
+++ src/component/content/PlantType.jsx
@@ -6,64 +6,64 @@ const PlantType = () => {
 
     return (
         <div className='w-full'>
-            <div className="mt-10 p-4 px-2 md:px-6 lg:px-16">
+            <div className="mt-10 lg:mt-20 p-4 px-2 md:px-6 lg:px-16">
                 <h3 className='text-2xl md:text-4xl font-bold text-center'>Nature's <span className="text-primary">Oxygen Hero</span>es</h3>
-                <p className="text-md text text-center font-medium  mt-2 py-4">Discover the magnificent plants that contribute the most to our environment by producing abundant oxygen, essential for life on Earth.</p>
+                <p className="text-md text text-center font-medium  mt-2 py-2">Discover the magnificent plants that contribute the most to our environment by producing abundant oxygen, essential for life on Earth.</p>
 
-                <div className="flex flex-col lg:flex-row py-1 mt-4">
+                <div className="flex flex-col lg:flex-row py-1 mt-0 lg:mt-4">
                     <div className="w-full lg:w-1/2 text-center p-2">
                         <img src={earth} alt="Earth" className='w-full' />
                     </div>
                     <div className="w-full lg:w-1/2 text-xs p-2 rounded-2xl md:mt-12">
-                        <div className="flex items-start">
-                            <span className="flex-shrink-0">
+                        <div className="flex items-start ">
+                            <span className="flex-shrink-0 border-4 border-green-700 p-1 rounded-full">
                                 <img src={tree1} alt="Banyan Tree" className='w-24 h-24 rounded-full object-cover' />
                             </span>
                             <span className='ps-3 '>
                                 <div className="text-lg text-primary font-semibold">Banyan Tree</div>
-                                <div className='text-sm'>The Banyan tree is a large, majestic tree known for its expansive canopy and complex aerial roots that descend from its branches to form additional trunks.</div>
+                                <div className='text-sm'>Known for its massive size and extensive canopy, the Banyan tree is a powerful oxygen producer, contributing significantly to the environment.</div>
                             </span>
                         </div>
 
                         <div className="flex py-1 ms-0 sm:ms-20 items-start">
-                            <span className="flex-shrink-0">
+                            <span className="flex-shrink-0 border-4 border-green-700 p-1 rounded-full">
                                 <img src={tree1} alt="Banyan Tree" className='w-24 h-24 rounded-full object-cover' />
                             </span>
                             <span className='ps-3 '>
-                                <div className="text-lg text-primary font-semibold">Banyan Tree</div>
+                                <div className="text-lg text-primary font-semibold">Peepal Tree</div>
                                 <div className='text-sm'>
-                                    The Banyan tree is a large, majestic tree known for its expansive canopy and complex aerial roots that descend from its branches to form additional trunks.
+                                Known for its massive size and extensive canopy, the Banyan tree is a powerful oxygen producer, contributing significantly to the environment.
                                 </div>
                             </span>
                         </div>
 
-                        <div className="flex py-1 ms-0 sm:ms-32 items-start">
-                            <span className="flex-shrink-0">
+                        <div className="flex py-1 ms-0 sm:ms-32 items-start ">
+                            <span className="flex-shrink-0 border-4 border-green-700 p-1 rounded-full">
                                 <img src={tree1} alt="Banyan Tree" className='w-24 h-24 rounded-full object-cover' />
                             </span>
                             <span className='ps-3 '>
-                                <div className="text-lg text-primary font-semibold">Banyan Tree</div>
-                                <div className='text-sm'>The Banyan tree is a large, majestic tree known for its expansive canopy and complex aerial roots that descend from its branches to form additional trunks.</div>
+                                <div className="text-lg text-primary font-semibold">Neem Tree</div>
+                                <div className='text-sm'>Neem is a fast-growing tree that releases a large amount of oxygen, improving air quality.  It has significant medicinal properties, making it valuable for traditional medicine.</div>
                             </span>
                         </div>
 
                         <div className="flex py-1 ms-0 sm:ms-20 items-start">
-                            <span className="flex-shrink-0">
+                            <span className="flex-shrink-0 border-4 border-green-700 p-1 rounded-full">
                                 <img src={tree1} alt="Banyan Tree" className='w-24 h-24 rounded-full object-cover' />
                             </span>
                             <span className='ps-3 '>
-                                <div className="text-lg text-primary font-semibold">Banyan Tree</div>
-                                <div className='text-sm'>The Banyan tree is a large, majestic tree known for its expansive canopy and complex aerial roots that descend from its branches to form additional trunks.</div>
+                                <div className="text-lg text-primary font-semibold">Arjuna Tree </div>
+                                <div className='text-sm'>Arjuna is known for its high oxygen release, contributing to better air quality. Traditionally used in Ayurvedic medicine, the Arjuna tree is valued for its therapeutic properties, especially for heart health.</div>
                             </span>
                         </div>
 
                         <div className="flex py-1 items-start">
-                            <span className="flex-shrink-0">
+                            <span className="flex-shrink-0 border-4 border-green-700 p-1 rounded-full">
                                 <img src={tree1} alt="Banyan Tree" className='w-24 h-24 rounded-full object-cover' />
                             </span>
                             <span className='ps-3 '>
-                                <div className="text-lg text-primary font-semibold">Banyan Tree</div>
-                                <div className='text-sm'>The Banyan tree is a large, majestic tree known for its expansive canopy and complex aerial roots that descend from its branches to form additional trunks.</div>
+                                <div className="text-lg text-primary font-semibold">Sandalwood Tree </div>
+                                <div className='text-sm'>Sandalwood trees are known for their oxygen-releasing properties, especially in their mature stages. The wood is highly valued for its fragrance and is used in a variety of products, from perfumes to religious rituals.</div>
                             </span>
                         </div>
                     </div>
