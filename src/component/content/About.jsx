553937f634357789ff3cba061ba77058bea76dc8
testuser <testuser@gmail.com>
2024-08-27 18:27:22 +0530

    about

--- src/component/content/About.jsx
+++ src/component/content/About.jsx
@@ -1,13 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-
-
-const About = () => {
-  return (
-    <div className=''>
- 
-=======
-<<<<<<< HEAD
 import AboutIntro from './AboutIntro'
 import AboutVision from './AboutVision'
 import Mission from './Mission'
@@ -18,16 +9,7 @@ const About = () => {
     <AboutIntro/>
     <AboutVision/>
     <Mission/>
-=======
-import Gallery from './Gallery'
-
-const About = () => {
-  return (
-    <div className='mt-24'>
-    <Gallery/>
->>>>>>> c95a9fe7d68488579a0f80bb8290060aa2e041e0
->>>>>>> fbe8f61285d08ca1387514d4ae3ecfdce87aa09e
-    </div>
+</div>
   )
 }
 
