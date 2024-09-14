7ce32214fc1bd7f5e7bee1f3830536cb5d8b1297
testuser <testuser@gmail.com>
2024-09-14 18:30:20 +0530

    info and imgae

---
+++ src/component/content/LandDonate.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DonateOptions from './DonateOptionS.JSX'
+
+const LandDonate = () => {
+  return (
+    <div>
+<DonateOptions heading="Land Donate"/>
+
+
+    </div>
+  )
+}
+
+export default LandDonate
---
+++ src/component/content/TreeDonate.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DonateOptions from './DonateOptionS.JSX'
+
+
+const TreeDonate = () => {
+  return (
+    <div>
+
+<DonateOptions heading="Donate Tree"/>
+
+
+    </div>
+  )
+}
+
+export default TreeDonate
