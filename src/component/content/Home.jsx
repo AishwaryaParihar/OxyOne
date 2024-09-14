11b561d6bd9265529248fc396f6caa48d2d94a45
testuser <testuser@gmail.com>
2024-09-14 13:54:08 +0530

    summary

--- src/component/content/Home.jsx
+++ src/component/content/Home.jsx
@@ -6,15 +6,19 @@ import PlantType from './PlantType'
 import Testimonials from './Testimonials'
 import OurImpact from './OurImpact'
 import Mission from './Mission'
+import Demoo from './Demoo'
+import GiftorDonate from './GiftorDonate'
 const Home = () => {
   return (
     <div >
       <Hero/>
       <LeaderBoard/>
       <PlantType/>
+      <GiftorDonate/>
       <Mission/>
       <OurImpact/>
       <Testimonials/>
+      <Demoo age={14} name={'toshi'} pancard={false}/>
     </div>
   )
 }
